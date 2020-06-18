ba4f6a93e2b9a5d4826889fc99c4e0b12ba0b7ca
Leo <leoskywork@outlook.com>
2020-06-18 22:00:34 +0800

    minor ui update

--- src/components/routines/FulfillmentView.js
+++ src/components/routines/FulfillmentView.js
@@ -31,8 +31,8 @@ export class FulfillmentView extends React.Component {
             <span>&nbsp;&nbsp;</span>
             <span>{this.getLastFulfillDescription()}</span>
             <span>&nbsp;&nbsp;</span>
-            <button className={this.getFulfillExpandButtonStyle()} onClick={this.onToggleLskFulfill}>...</button>
-            <button className='btn-intro-fulfill' onClick={() => { }}>x</button>
+            <button className={this.getFulfillExpandButtonStyle()} onClick={this.onToggleLskFulfill}>{this.state.collapseView ? 'v' : '^'}</button>
+            <button className='btn-intro-fulfill no-bg-color' onClick={() => { }}>...</button>
             <form className="intro-fulfill-form" onSubmit={e => this.onSubmitFulfillment(e)} hidden={this.state.collapseView}>
                 <input
                     type="text"
